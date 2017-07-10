89a9a438ca6d6df364266423351485c8ae111f49
Rohde Felipe <fr.rohde@gmail.com>
2017-07-10 17:11:10 -0300

    Updating browsersync

--- gulp/browserSync.js
+++ gulp/browserSync.js
@@ -4,7 +4,7 @@ export default function(gulp, plugins, args, config, taskTarget, browserSync) {
   // BrowserSync
   gulp.task('browserSync', () => {
 		browserSync.init(['./css/*.css', './js/build/*.js'], {
-		  proxy: "itelios.domusvi.site.docksal",
+		  proxy: "drupal7dummy.docksal",
 		  port: config.port || 3000,
       open: args.open ? 'local' : false,
 		});
